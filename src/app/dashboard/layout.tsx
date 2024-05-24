d8beee415f5a33569ca3ff2a6289f73eee9b7498
Tiago Marques <tfcmarques@proton.me>
2024-05-24 14:43:20 +0100

    Add breadcrumb + new account page + favicon

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,12 @@
-import { LayoutDashboard, Coins, Settings, ArrowRightLeft } from "lucide-react";
+"use client";
+
+import {
+  ArrowRightLeft,
+  Coins,
+  LayoutDashboard,
+  LucideIcon,
+  UserRound,
+} from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Tooltip,
@@ -6,78 +14,121 @@ import {
   TooltipTrigger,
   TooltipProvider,
 } from "@/components/ui/tooltip";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { ThemeToggle } from "@/components/theme/theme-toggle";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { capitalize } from "lodash";
+import React from "react";
+
+interface NavItem {
+  label: string;
+  href: string;
+  icon: LucideIcon;
+}
+
+const navItems: NavItem[] = [
+  {
+    label: "Dashboard",
+    href: "/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    label: "Transactions",
+    href: "/dashboard/transactions",
+    icon: ArrowRightLeft,
+  },
+  {
+    label: "Account",
+    href: "/dashboard/account",
+    icon: UserRound,
+  },
+];
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const path: string = usePathname();
+  const breadcrumbs: string[] = path.split("/").filter((crumb) => crumb !== "");
+
   return (
     <div className="grid h-screen w-full pl-[56px]">
       <aside className="inset-y fixed left-0 z-20 flex h-full flex-col border-r">
         <div className="border-b h-[57px] flex items-center justify-center">
           <Coins className="size-6" />
         </div>
-        <nav className="grid gap-1 p-2">
+        <nav className="grid gap-2 p-2">
           <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Dashboard"
-                >
-                  <LayoutDashboard className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Dashboard
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Transactions"
-                >
-                  <ArrowRightLeft className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Transactions
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </nav>
-        <nav className="mt-auto grid gap-1 p-2">
-          <TooltipProvider>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="rounded-lg"
-                  aria-label="Settings"
-                >
-                  <Settings className="size-5" />
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent side="right" sideOffset={5}>
-                Settings
-              </TooltipContent>
-            </Tooltip>
+            {navItems.map((item, index) => {
+              const NavItemIcon = item.icon;
+
+              return (
+                <Tooltip key={index}>
+                  <TooltipTrigger asChild>
+                    <Link href={item.href}>
+                      <Button
+                        aria-label={item.label}
+                        variant="ghost"
+                        size="icon"
+                        className={cn(
+                          "rounded-lg",
+                          path === item.href ? "bg-accent" : "transparent"
+                        )}
+                      >
+                        <NavItemIcon className="size-5" />
+                      </Button>
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent side="right" sideOffset={5}>
+                    {item.label}
+                  </TooltipContent>
+                </Tooltip>
+              );
+            })}
           </TooltipProvider>
         </nav>
       </aside>
       <div className="flex flex-col">
         <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-8">
-          <h1 className="text-xl font-semibold">Dashboard</h1>
+          <Breadcrumb>
+            <BreadcrumbList>
+              {breadcrumbs.map((crumb, index) => {
+                const href = `/${breadcrumbs.slice(0, index + 1).join("/")}`;
+                const isLast = index === breadcrumbs.length - 1;
+
+                if (isLast) {
+                  return (
+                    <BreadcrumbItem key={index}>
+                      <BreadcrumbPage className="font-semibold">
+                        {capitalize(crumb)}
+                      </BreadcrumbPage>
+                    </BreadcrumbItem>
+                  );
+                }
+
+                return (
+                  <React.Fragment key={index}>
+                    <BreadcrumbItem>
+                      <BreadcrumbLink href={href}>
+                        {capitalize(crumb)}
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                  </React.Fragment>
+                );
+              })}
+            </BreadcrumbList>
+          </Breadcrumb>
           <ThemeToggle />
         </header>
         <main>{children}</main>
